81364003589dd6b49e8f03a44d25046f6b7bf7a8
Tartur-dev <arthurbarbera5@gmail.com>
2022-01-22 22:41:15 +0100

    Update

--- src/main/java/fr/tartur/loupgarou/LoupGarou.java
+++ src/main/java/fr/tartur/loupgarou/LoupGarou.java
@@ -2,6 +2,7 @@ package fr.tartur.loupgarou;
 
 import fr.tartur.loupgarou.commands.SetupCommand;
 import fr.tartur.loupgarou.commands.UnSetupCommand;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 
@@ -11,27 +12,12 @@ public class LoupGarou {
 
     public static void main(String[] args) {
 
-        DiscordApi api = new DiscordApiBuilder().setToken(getToken()).login().join();
+        Dotenv dotenv = Dotenv.load();
+
+        DiscordApi api = new DiscordApiBuilder().setToken(dotenv.get("TOKEN")).login().join();
         api.addMessageCreateListener(new SetupCommand(api));
         api.addMessageCreateListener(new UnSetupCommand(api));
 
-    }
-
-    private static String getToken() {
-        String path = "C:/Users/Arthur/Documents/Développement/discbots-token/loup-garou.txt";
-        System.out.println(path);
-        File token = new File(path);
-
-        if (!token.exists()) {
-            throw new RuntimeException("Le token du bot Discord n'a pas pu être trouvé ! :(");
-        }
-
-        try {
-            return new BufferedReader(new FileReader(token)).readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
     }
 
 }
